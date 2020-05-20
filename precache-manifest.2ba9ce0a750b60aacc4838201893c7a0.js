da527058e6c05c1854393cadf87085a94943aa74
Lucas Dias <48134977+lucaspdp@users.noreply.github.com>
2020-05-19 21:09:26 -0300

    Updates

---
+++ precache-manifest.2ba9ce0a750b60aacc4838201893c7a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113de6d32e1f61bbbf7dc50afbdf3246",
+    "url": "/importcenter-frontend/index.html"
+  },
+  {
+    "revision": "ca846656f34e7da2df20",
+    "url": "/importcenter-frontend/static/js/2.6366bbd5.chunk.js"
+  },
+  {
+    "revision": "6bf4bd0b45c98d6a050c",
+    "url": "/importcenter-frontend/static/js/main.c41914bc.chunk.js"
+  },
+  {
+    "revision": "18b4462a5c2912ec54c9",
+    "url": "/importcenter-frontend/static/js/runtime-main.659752f3.js"
+  },
+  {
+    "revision": "ac9de67045c971b06bf1b4a814350e4e",
+    "url": "/importcenter-frontend/static/media/Bandeira.ac9de670.jpg"
+  },
+  {
+    "revision": "34c548f2c238e992702cf0239ac5ba4e",
+    "url": "/importcenter-frontend/static/media/D-Icon.34c548f2.png"
+  },
+  {
+    "revision": "48e74cdcc2e9dc6e6535e5ccbe038b7e",
+    "url": "/importcenter-frontend/static/media/Dimsport-logo.48e74cdc.png"
+  },
+  {
+    "revision": "66fb3a9181d6de2d0224bdc36668b3b7",
+    "url": "/importcenter-frontend/static/media/alientech.66fb3a91.png"
+  },
+  {
+    "revision": "fa766f30d6b21953e1b8cb813e852a03",
+    "url": "/importcenter-frontend/static/media/import-center.fa766f30.png"
+  }
+]);
